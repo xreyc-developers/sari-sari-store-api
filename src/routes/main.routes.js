36f30904ebfb7b0cc8da58d594f8f6403939b96f
xreyc-developers <segumareyco@gmail.com>
2022-06-14 16:53:54 +0800

    api name

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -3,7 +3,7 @@ const router = require('express-promise-router')();
 /**
  * @description Authentication Routes
  */
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
     return res.status(200).json({
         name: "Sari-sari-store-api",
         vesion: 0.1
