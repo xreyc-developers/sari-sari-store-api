7e7448b16a46077257e4139533e8ad35b6c4e6c6
xreyc-developers <segumareyco@gmail.com>
2022-06-26 18:09:11 +0800

    test

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -10,4 +10,11 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login/', (req, res) => {
+    return res.status(200).json({
+        name: "Sari-sari-store-api",
+        vesion: 0.1
+    });
+});
+
 module.exports = router;
